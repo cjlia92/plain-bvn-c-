e66e210d1b139129f9e2c706003609965454b55b
psaI2332 <yydz_jdw_dog@mail.nankai.edu.cn>
2023-05-03 21:52:50 +0800

    Be able to do easy attack. Solved the following BUG: 1.cannot jump when runnning after a

--- Project7/源.cpp
+++ Project7/源.cpp
@@ -1,20 +1,48 @@
 #include<graphics.h>
 #include<conio.h>
 #include <iostream>
-#define key_down(D) ((GetAsyncKeyState('D'))?0:1)
 using namespace std;
-static int  Isjumping = 0;
-IMAGE background, stillb[4], stilla[4], runb[8], runa[8], jumpa, jumpb;
 struct role
 {
     int x = 10;
     int y = 600;
-}role;
-void PrintJump();
-int IsJump();
-int IsGround();
-int IsRun();
-void Initial()
+    int height = -43;
+    int width = 38;
+};
+struct Attackwind
+{
+    int x;
+    int y;
+    int width;
+    int height;
+};
+class Role {
+public:
+    role player;
+    Role(int a, int b, int c, int d)
+    {
+        player.x = a;
+        player.y = b;
+        player.height = c;
+        player.width = d;
+    }
+    int Isjumping = 0;//Jugde how many times the role has jumped.
+    int Isattacking = 0;//Jugde how many times the role has attacked.
+    void Initial();
+    void PrintBackground();
+    void PrintStill();
+    int IsJump();
+    int IsJumping();
+    int IsGround();
+    int IsRun();
+    void PrintRun();
+    void PrintJump();
+    int IsAttack();
+    void PrintAttack();
+    void PrintMove();
+};
+IMAGE background, stillb[4], stilla[4], runb[8], runa[8], jumpa, jumpb, attacka[3], attackb[3];
+void Role::Initial()
 {
     loadimage(&background, _T("pictures\\background\\0.png"));
 
@@ -48,12 +76,19 @@ void Initial()
 
     loadimage(&jumpa, _T("pictures\\jump\\a.png"));
     loadimage(&jumpb, _T("pictures\\jump\\b.png"));
+
+    loadimage(&attacka[0], _T("pictures\\attack\\a0.png"));
+    loadimage(&attackb[0], _T("pictures\\attack\\b0.png"));
+    loadimage(&attacka[1], _T("pictures\\attack\\a1.png"));
+    loadimage(&attackb[1], _T("pictures\\attack\\b1.png"));
+    loadimage(&attacka[2], _T("pictures\\attack\\a2.png"));
+    loadimage(&attackb[2], _T("pictures\\attack\\b2.png"));
 }
-void PrintBackground()
+void Role::PrintBackground()
 {
     putimage(0, 0, &background);
 }
-void PrintStill() 
+void Role::PrintStill()
 {
     for (int i = 0; i < 4; i++)
     {
@@ -61,77 +96,83 @@ void PrintStill()
         {
             return;
         }
-        if (IsJump()) 
+        if (IsJump())
         {
             return;
         }
         BeginBatchDraw();
         PrintBackground();
-        putimage(role.x, role.y, &stillb[i], SRCAND);
-        putimage(role.x, role.y, &stilla[i], SRCPAINT);
+        putimage(player.x, player.y, &stillb[i], SRCAND);
+        putimage(player.x, player.y, &stilla[i], SRCPAINT);
         EndBatchDraw();
         Sleep(100);
     }
 }
-int IsJump()
+int Role::IsJump()
 {
-   
-    if (GetAsyncKeyState('K')&&Isjumping<1)
+
+    if (GetAsyncKeyState('K') && Isjumping < 1)
     {
         Isjumping++;
-        IsGround(); 
+        IsGround();
         return 1;
     }
     return 0;
 }
-int IsJumping()
+int Role::IsJumping()
 {
-    if (role.y < 600)
+    if (player.y < 600)
     {
         return 1;
     }
     return 0;
 }
-int IsGround()
+int Role::IsGround()
 {
-    if (role.y == 600)
+    if (player.y == 600)
     {
         Isjumping = 0;
         return 1;
     }
     return 0;
 }
-int IsRun()
+int Role::IsRun()
 {
     if (GetAsyncKeyState('D'))
     {
         return 1;
     }
-    if (GetAsyncKeyState('A') )
+    if (GetAsyncKeyState('A'))
     {
         return 1;
     }
     return 0;
 }
-void PrintRun()
+void Role::PrintRun()
 {
     for (int i = 0; i < 8; i++)
     {
-        
-        if(IsRun())
+
+        if (IsRun())
         {
             BeginBatchDraw();
             PrintBackground();
             if (GetAsyncKeyState('D'))
             {
-                role.x += 15;
+                if (player.x < 1232)
+                {
+                    player.x += 15;
+                }
             }
             if (GetAsyncKeyState('A'))
             {
-                role.x -= 15;
+                if (player.x > 10)
+                {
+                    player.x -= 15;
+                }
             }
-            putimage(role.x, role.y, &runb[i], SRCAND);
-            putimage(role.x, role.y, &runa[i], SRCPAINT);
+            putimage(player.x, player.y, &runb[i], SRCAND);
+            putimage(player.x, player.y, &runa[i], SRCPAINT);
             EndBatchDraw();
             Sleep(50);
         }
@@ -142,88 +183,206 @@ void PrintRun()
         }
     }
 }
-
-inline void PrintJump()
+void Role::PrintJump()
 {
     for (int i = 0; i < 8; i++)
     {
-        role.y -= 18;
+        player.y -= 18;
         if (GetAsyncKeyState('D'))
         {
-            role.x += 15;
+            player.x += 15;
         }
         if (GetAsyncKeyState('A'))
         {
-            role.x -= 15;
+            player.x -= 15;
         }
         BeginBatchDraw();
         PrintBackground();
-        putimage(role.x, role.y, 43, 49, &jumpb, 43 * (i % 2), 0, SRCAND);
-        putimage(role.x, role.y, 43, 49, &jumpa, 43 * (i % 2), 0, SRCPAINT);
+        putimage(player.x, player.y, 43, 49, &jumpb, 43 * (i % 2), 0, SRCAND);
+        putimage(player.x, player.y, 43, 49, &jumpa, 43 * (i % 2), 0, SRCPAINT);
         EndBatchDraw();
-        Sleep(25);       
+        Sleep(25);
     }
     if (IsJump())
     {
         PrintJump();
-    } 
-    for (int i =0; i < 7; i++)
+    }
+    for (int i = 0; i < 7; i++)
     {
         if (IsJump())
         {
             PrintJump();
         }
-        role.y += 18;
+        player.y += 18;
         if (GetAsyncKeyState('D'))
         {
-            role.x += 15;
+            player.x += 15;
         }
         if (GetAsyncKeyState('A'))
         {
-            role.x -= 15;
+            player.x -= 15;
         }
         BeginBatchDraw();
         PrintBackground();
-        putimage(role.x, role.y, 43, 49, &jumpb, 43 * 2, 0, SRCAND);
-        putimage(role.x, role.y, 43, 49, &jumpa, 43 * 2, 0, SRCPAINT);
+        putimage(player.x, player.y, 43, 49, &jumpb, 43 * 2, 0, SRCAND);
+        putimage(player.x, player.y, 43, 49, &jumpa, 43 * 2, 0, SRCPAINT);
         EndBatchDraw();
         Sleep(25);
     }
-    role.y += 18;
+    player.y += 18;
     BeginBatchDraw();
     PrintBackground();
-    putimage(role.x, role.y, 43, 49, &jumpb, 43 * 3, 0, SRCAND);
-    putimage(role.x, role.y, 43, 49, &jumpa, 43 * 3, 0, SRCPAINT);
+    putimage(player.x, player.y, 43, 49, &jumpb, 43 * 3, 0, SRCAND);
+    putimage(player.x, player.y, 43, 49, &jumpa, 43 * 3, 0, SRCPAINT);
     EndBatchDraw();
     Sleep(25);
 }
-void PrintMove()
+int Role::IsAttack()
 {
+    if (GetAsyncKeyState('J'))
+    {
+        return 1;
+    }
+    return 0;
+}
+void Role::PrintAttack()
+{
+    //the first attack.
+    for (int i = 0; i < 4; i++) {
+        player.x += 3;
+        BeginBatchDraw();
+        PrintBackground();
+        if (i == 0 || i == 3)
+        {
+            putimage(player.x, player.y, 34, 48, &attackb[0], 155 * (i % 2), 0, SRCAND);
+            putimage(player.x, player.y, 34, 48, &attacka[0], 155 * (i % 2), 0, SRCPAINT);
+        }
+        if (i == 1 || i == 2)
+        {
+            putimage(player.x, player.y, 50, 48, &attackb[0], 40 + 57 * ((i + 1) % 2), 0, SRCAND);
+            putimage(player.x, player.y, 50, 48, &attacka[0], 40 + 57 * ((i + 1) % 2), 0, SRCPAINT);
+        }
+        EndBatchDraw();
+        Sleep(150);
+    }
+    //the second attack.
+    if (IsAttack())
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            player.x += 3;
+            BeginBatchDraw();
+            PrintBackground();
+            if (i == 0 || i == 1)
+            {
+                putimage(player.x, player.y, 48, 47, &attackb[1], 48 * (i % 2), 0, SRCAND);
+                putimage(player.x, player.y, 48, 47, &attacka[1], 48 * (i % 2), 0, SRCPAINT);
+            }
+            if (i == 2)
+            {
+                putimage(player.x, player.y, 60, 47, &attackb[1], 106, 0, SRCAND);
+                putimage(player.x, player.y, 60, 47, &attacka[1], 106, 0, SRCPAINT);
+            }
+            if (i == 3)
+            {
+                putimage(player.x, player.y, 50, 47, &attackb[1], 175, 0, SRCAND);
+                putimage(player.x, player.y, 50, 47, &attacka[1], 175, 0, SRCPAINT);
+            }
+            if (i == 4)
+            {
+                putimage(player.x, player.y, 33, 47, &attackb[1], 233, 0, SRCAND);
+                putimage(player.x, player.y, 33, 47, &attacka[1], 233, 0, SRCPAINT);
+            }
+            EndBatchDraw();
+            Sleep(110);
+        }
+    }
+    else
+    {
+        return;
+    }
+    //the third attack.
+    if (IsAttack())
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            if (i >= 2 && i <= 4)
+            {
+                player.x += 20;
+            }
+            BeginBatchDraw();
+            PrintBackground();
+            if (i == 0 || i == 1 || i == 2)
+            {
+                putimage(player.x, player.y, 46, 44, &attackb[2], 46 * (i % 3), 0, SRCAND);
+                putimage(player.x, player.y, 46, 44, &attacka[2], 46 * (i % 3), 0, SRCPAINT);
+            }
+            if (i == 3)
+            {
+                putimage(player.x, player.y, 62, 44, &attackb[2], 138, 0, SRCAND);
+                putimage(player.x, player.y, 62, 44, &attacka[2], 138, 0, SRCPAINT);
+            }
+            if (i == 4)
+            {
+                putimage(player.x, player.y, 55, 44, &attackb[2], 209, 0, SRCAND);
+                putimage(player.x, player.y, 55, 44, &attacka[2], 209, 0, SRCPAINT);
+            }
+            if (i == 5)
+            {
+                putimage(player.x, player.y, 48, 44, &attackb[2], 283, 0, SRCAND);
+                putimage(player.x, player.y, 48, 44, &attacka[2], 283, 0, SRCPAINT);
+            }
+            if (i == 6)
+            {
+                putimage(player.x, player.y, 38, 44, &attackb[2], 331, 0, SRCAND);
+                putimage(player.x, player.y, 38, 44, &attacka[2], 331, 0, SRCPAINT);
+            }
+            EndBatchDraw();
+            Sleep(100);
+        }
+    }
+    else
+    {
+        return;
+    }
+}
+void Role::PrintMove()
+{
+    IsGround();
     if (IsRun())
     {
         PrintRun();
     }
-    else if (IsJump()) {
+    else if (IsJump())
+    {
         PrintJump();
+
+    }
+    else if (IsAttack())
+    {
+        PrintAttack();
     }
     else
     {
-        if(IsGround())
+        if (IsGround())
         {
             PrintStill();
         }
     }
     return;
 }
-int main() 
+
+int main()
 {
-    Initial();
-    initgraph(1280,720);
-    while(1)
+    Role A(10, 600, -43, 38);
+    A.Initial();
+    initgraph(1280, 720);
+    while (1)
     {
-        PrintBackground();
-        PrintMove();
+        A.PrintBackground();
+        A.PrintMove();
     }
     system("pause");
-	return 0;
+    return 0;
 }
